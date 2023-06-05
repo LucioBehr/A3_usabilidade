a29a6aabe686f341062166b066416053fb0b49ed
LucioBehr <lucio.behr@outlook.com>
2023-06-05 20:49:19 -0300

    commit 01 050623 Implementacao de ejs no index. itens aparecendo no index.

--- catalogo.js
+++ catalogo.js
@@ -20,42 +20,57 @@ app.use(cors({
 app.use(bodyParser.urlencoded({extended: true}))
 
 var server = http.createServer(app);
-app.get('/', function(req, res){
-    //Se digitarmos no navegador localhost:3333, faremos uma requisição GET
-    res.sendFile(path.join(__dirname,'../A3_USABILIDADE/index.html'));
+app.get('/', function(req, res) {
+    db.all('SELECT id, nome FROM Item', function(err, rows) {
 
+            // Itens encontrados, renderize a página de resultados da busca
+            res.render('index', { items: rows });
+        
+    });
 });
+
+
 
     //Adicionar um novo item.
     app.post('/add', function(req, res){
-        //res.sendFile(path.join(__dirname,'../A3_USABILIDADE/views/add.ejs'));
-        db.serialize(()=>{
-            db.run('INSERT INTO Item(id, nome, preco, desc)VALUES(?, ?,?,?)',
-            [req.body.id, req.body.nome, req.body.preco, req.body.desc],
-            function(err){
-                if (err){
-                    return console.log(err.message);
+        db.serialize(() => {
+            // Verificar se o ID já está cadastrado
+            db.get('SELECT id FROM Item WHERE id = ?', req.body.id, function(err, row) {
+                if (err) {
+                    return console.error(err.message);
                 }
-
-                res.render('add', { id: req.body.id, nome: req.body.nome });
-            
-                    
+    
+                if (row) {
+                    // ID já está cadastrado, renderizar página de erro
+                    res.render('errorS', { message: 'ID já está cadastrado' });
+                } else {
+                    // ID não está cadastrado, inserir o novo item
+                    db.run('INSERT INTO Item(id, nome, preco, desc) VALUES (?, ?, ?, ?)',
+                        [req.body.id, req.body.nome, req.body.preco, req.body.desc],
+                        function(err) {
+                            if (err) {
+                                return console.log(err.message);
+                            }
+    
+                            res.render('add', { id: req.body.id, nome: req.body.nome });
+                        }
+                    );
+                }
             });
         });
     });
     app.post('/search', function(req, res) {
-        db.get('SELECT id, nome FROM Item WHERE id = ?', [req.body.scid], function(err, row) {
-
+        db.all('SELECT id, nome FROM Item WHERE id = ? OR nome LIKE ?', [req.body.scid, '%' + req.body.scid + '%'], function(err, rows) {
             if (err) {
-                res.send("Erro ao encontrar item");
+                res.send("Erro ao encontrar itens");
                 return console.error(err.message);
             }
     
-            if (!row) {
-                res.render('errorS');
+            if (!rows || rows.length === 0) {
+                res.render('errorS', { message: 'Produto não encontrado' });
             } else {
-                // Item encontrado, renderize a página de resultados da busca
-                res.render('search', { id: row.id, nome: row.nome });
+                // Itens encontrados, renderize a página de resultados da busca
+                res.render('search', { items: rows , id: req.body.id, nome: req.body.nome});
             }
         });
     });
@@ -72,7 +87,7 @@ app.get('/', function(req, res){
                 if (err){
                     return console.log(err.message);
                 }
-    
+
                 res.render('alter', { id: req.body.id, nome: req.body.nome });
             
                     
@@ -81,12 +96,27 @@ app.get('/', function(req, res){
     });
 
     app.post('/remove', function(req, res) {
-        db.serialize(()=>{
+        db.serialize(() => {
             db.run('DELETE FROM Item WHERE id = ?', [req.body.id], function(err) {
-                if (err){
+                if (err) {
                     return console.log(err.message);
                 }
-                res.sendFile(path.join(__dirname,'../A3_USABILIDADE/index.html'));
+    
+                // Realize a consulta ao banco de dados novamente para obter a lista atualizada de itens
+                db.all('SELECT id, nome FROM Item', function(err, rows) {
+                    if (err) {
+                        console.error(err.message);
+                        res.send("Erro ao encontrar itens");
+                        return;
+                    }
+    
+                    if (!rows || rows.length === 0) {
+                        res.render('index', { items: rows });
+                    } else {
+                        // Itens encontrados, renderize a página de resultados da busca com os itens atualizados
+                        res.render('index', { items: rows });
+                    }
+                });
             });
         });
     });
